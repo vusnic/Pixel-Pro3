b21aeb9ceb9d2dc7b28314b91b9eaa1578908890
Vinicius Assad de Magalhaes <121522994+vusnic@users.noreply.github.com>
2026-02-08 22:33:35 -0600

    Added app.html

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -6,10 +6,15 @@
     <title>PixelPro3 - Agência Digital</title>
     <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
-    @vite(['resources/css/app.css'])
+    @if (file_exists(public_path('hot')))
+        <script type="module" src="http://localhost:5173/@vite/client"></script>
+        <script type="module" src="http://localhost:5173/src/main.tsx"></script>
+    @else
+        <link rel="stylesheet" href="{{ asset('build/assets/app-L6-oVGfG.css') }}">
+        <script type="module" src="{{ asset('build/assets/main.js') }}"></script>
+    @endif
   </head>
   <body>
     <div id="root"></div>
-    @vite(['src/main.tsx'])
   </body>
 </html>
